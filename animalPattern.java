d68c2fd8ca4c627c7b76819c5a4b047231b5512a
Ashish Khanal <ashish_0320@deerwalk.edu.np>
2015-01-19 14:21:38 +0545

    new

---
+++ animalPattern.java
@@ -0,0 +1,15 @@
+/**
+ * Created by Ashish on 1/19/2015.
+ */
+public class animalPattern {
+    public static void main(String[]args){
+        System.out.println("          '__'");
+        System.out.println("         (Â©Â©)");
+        System.out.println("  /========\\/");
+        System.out.println(" / || %% ||");
+        System.out.println("*  ||----||");
+        System.out.println("   Â¥Â¥   Â¥Â¥");
+        System.out.println("   \"\"    \"\"");
+    }
+
+}
